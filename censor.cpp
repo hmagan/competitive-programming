3aaa261f635ab2527c3e6e944c7b94fb25ea9609
Hank Magan <bryant.magan@gmail.com>
2021-03-28 23:56:48 -0400

    bronze practice

--- censor.cpp
+++ censor.cpp
@@ -6,30 +6,23 @@ int main()
 {
 	ifstream fin ("censor.in");
 	ofstream fout ("censor.out");
-	//ios::sync_with_stdio(0);
-	//cin.tie(0);
 	
 	string word;
-	string text;
+	fin >> word;
 	
-	fin >> text >> word;
+	string censor;
+	fin >> censor;
 	
-	int len = word.length();
-	
-	int idx = 0;
-	while(idx < text.length() - len + 1){
-		if(text.substr(idx, len) == word){
-			text = text.substr(0, idx) + text.substr(idx + len, text.length() - 1);
-			idx -= len;
-			if(idx < 0){
-				idx = 0;
+	for(int i = 0; i < word.length() - censor.length(); i++){
+		if(word.substr(i, censor.length()) == censor){
+			word = word.substr(0, i) + word.substr(i + censor.length(), word.length() - i + censor.length());
+			i -= censor.length();
+			if(i < 0){
+				i = 0;
 			}
-		} else {
-			idx++;
 		}
 	}
-	
-	fout << text;
+	fout << word;
 	
 	return 0;
 }
